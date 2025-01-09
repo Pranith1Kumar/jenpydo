239778f39194d06cd72dba0239b344bbce84e556
Ellendula Pranith Kumar <132734438+Pranith1Kumar@users.noreply.github.com>
2025-01-09 12:33:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,10 @@
-# app.py
-from flask import Flask, request, jsonify
+# calculator.py
+def add(x, y):
+    return x + y
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Basic Calculator API"
-
-@app.route('/add', methods=['POST'])
-def add():
-    data = request.json
-    return jsonify({"result": data["a"] + data["b"]})
-
-@app.route('/subtract', methods=['POST'])
-def subtract():
-    data = request.json
-    return jsonify({"result": data["a"] - data["b"]})
-
-@app.route('/multiply', methods=['POST'])
-def multiply():
-    data = request.json
-    return jsonify({"result": data["a"] * data["b"]})
-
-@app.route('/divide', methods=['POST'])
-def divide():
-    data = request.json
-    if data["b"] == 0:
-        return jsonify({"error": "Division by zero is not allowed"})
-    return jsonify({"result": data["a"] / data["b"]})
+def multiply(x, y):
+    return x * y
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    print("Addition:", add(5, 3))
+    print("Multiplication:", multiply(5, 3))
